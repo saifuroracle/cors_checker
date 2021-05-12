72d9d88e225d3e0e28f5bc06af6cd0c05b339511
saifur rahman <saifuroracle@gmail.com>
2021-05-12 18:56:16 +0600

    cors updated

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,134 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <v-container>
+        <v-sheet
+                class="mx-auto pa-12 ma-12"
+                elevation="0"
+                 color="grey lighten-5"
+            >
+
+            <v-layout row wrap align-center>
+                <v-flex>
+                    <v-card width="400" class="mx-auto" floating>
+                        <v-card-title>
+                            <h1 class="display-1 mx-auto font-weight-light">Login</h1>
+                            
+                        </v-card-title>
+
+                        <v-card-text>
+                            
+                            <v-form  @submit.prevent="loginPost()" >
+                                <v-text-field
+                                    name="email"
+                                    label="Email"
+                                    id="email"
+                                    v-model="loginDetails.email"
+                                    :rules="emailRules"
+                                ></v-text-field>
+                                <v-text-field
+                                    name="password"
+                                    label="Password"
+                                    id="password"
+                                    v-model="loginDetails.password"
+                                    :type="showPassword ? 'text' : 'password'"
+                                    :rules="passwordRules"
+
+                                ></v-text-field>
+
+                                <v-divider></v-divider>
+                                <p v-if="loginDetailsError.error" class="text-danger mt-1 red--text lighten-1 text-center">{{ loginDetailsError.loginError }}</p>
+                                <p v-if="loginDetailsValid.valid" class="text-success mt-1 green--text lighten-1 text-center">{{ loginDetailsValid.validMessage }}</p>
+
+                                <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                    <v-btn color="success" type="submit" text @click="loading=true">
+                                        Sign In
+                                    </v-btn>
+                                    
+                                </v-card-actions>
+                                <v-chip outlined small>api(post): {{api}}</v-chip>
+
+                            </v-form>
+                        </v-card-text>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+
+            <v-dialog v-model="loading" fullscreen >
+                <v-container fluid fill-height style="background-color: rgba(255, 255, 255, 0.5);">
+                <v-layout justify-center align-center>
+                    <v-progress-circular
+                    indeterminate
+                    color="primary">
+                    </v-progress-circular>
+                </v-layout>
+                </v-container>
+            </v-dialog>
+
+
+        
+
+        </v-sheet>
+    </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  export default {
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    name: 'HelloWorld',
+
+    data: () => ({
+        showPassword:false,
+        loginDetails : {  email: '', password: '' },
+        loginDetailsError : { error:false, loginError: '' },
+        loginDetailsValid : { valid:false, validMessage: '' },
+        emailRules: [
+            v => !!v || 'E-mail is required',
+            v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+        ],
+        passwordRules: [
+            v => !!v || 'Password is required',
+        ],
+        loading:false,
+        api: 'http://localhost:8001/api/v1/login'
+    }),
+
+    mounted() {
+    },
+
+    methods: {
+
+      loginPost(){
+
+
+              var _this = this;
+              this.$http.post(this.api,  this.loginDetails)
+              .then(function () {
+                  console.log('success!');
+                //   console.log(response.data.access_token);
+                  // window.location.href = '/home/home?token='+response.data.access_token;
+                //   let token = response.data.access_token;
+                //   localStorage.setItem('token', token);
+                //   _this.$store.dispatch('checkIsLoggedIn')
+                  // window.location.href = '/';
+                  
+                  _this.loading=false;
+                  _this.loginDetailsError = { error:false, loginError: "" };
+                  _this.loginDetailsValid = { valid:true, validMessage: 'Successful! Please wait..' }
+                  
+                //   _this.$router.push('/admin/dashboard');
+                  // _this.$router.go(-1);
+              })
+              .catch(function (error) {
+
+                //   console.log('failure !');
+                  // console.log(error.response);
+                  // console.log(error.response.data.error);
+                  // _this.loginDetailsError = { error:true, loginError: error.response.data.error };
+                  _this.loginDetailsError = { error:true, loginError: error.response };
+                  _this.loading=false;
+
+              })
+      },
+    },
+  }
+</script>
